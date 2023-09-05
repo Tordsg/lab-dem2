fe7f1f4a75a4acfbaa0c580cbd8b8eb866598217
Tord Gunnarsli <tordsg@stud.ntnu.no>
2023-09-06 08:38:53 +1000

    updated script

--- slurm_script.sh
+++ slurm_script.sh
@@ -1,9 +1,14 @@
-!/bin/bash
-SBATCH --job-name=initial-test
-SBATCH --partition=gpu
-SBATCH --nodes=1
-SBATCH --ntasks=1
-SBATCH --ntasks-per-node=1
-SBATCH --cpus-per-task=1
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --job-name=test
+#SBATCH --cpus-per-task 1
+#SBATCH --mail-type=All
+#SBATCH --mail-user=t.gunnarsli@uq.net.au
+#SBATCH -o test_out.txt
+#SBATCH -e test_err.txt
+#SBATCH --partition=vgpu
+#SBATCH --gres=gpu:1
 
-python3 CIFAR10.py
+conda activate env1
+python ~/compute/lab-dem2/CIFAR10.py
