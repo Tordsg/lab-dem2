b0ff7c6de39922755ff1134adb37db93d91de43b
Tord Gunnarsli <tordsg@stud.ntnu.no>
2023-09-06 08:53:02 +1000

    added tests

---
+++ slurm_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --job-name=test
+#SBATCH --cpus-per-task 1
+#SBATCH --mail-type=All
+#SBATCH --mail-user=t.gunnarsli@uq.net.au
+#SBATCH -o test_out.txt
+#SBATCH -e test_err.txt
+#SBATCH --partition=vgpu
+#SBATCH --gres=gpu:1
+
+conda activate env1
+python ~/compute/lab-dem2/test.py
